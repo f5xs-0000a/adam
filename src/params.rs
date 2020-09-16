78d3e5d3c130f5551f817c770242f2ec1eb6e159
F5XS <f5xs.0000a@gmail.com>
2020-09-16 09:17:08 +0800

    Error fixes and cargo fmt

--- src/params.rs
+++ src/params.rs
@@ -2,19 +2,21 @@ use crate::FLOAT;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct AdamParams {
-    pub alpha: FLOAT,
+    pub alpha:   FLOAT,
     pub epsilon: FLOAT,
-    pub beta_1: FLOAT,
-    pub beta_2: FLOAT,
+    pub beta_1:  FLOAT,
+    pub beta_2:  FLOAT,
 }
 
 impl Default for AdamParams {
     fn default() -> Self {
-        alpha: 0.001,
-        epsilon: 0.00000001,
-        beta_1: 0.9,
-        beta_2: 0.999,
+        AdamParams {
+            alpha:   0.001,
+            epsilon: 0.00000001,
+            beta_1:  0.9,
+            beta_2:  0.999,
+        }
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,7 @@
-use crate::params::AdamParams;
-use crate::FLOAT;
+use crate::{
+    params::AdamParams,
+    FLOAT,
+};
 use thiserror::Error;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -7,12 +9,12 @@ use thiserror::Error;
 pub struct AdamState {
     m: Vec<FLOAT>, // has a length of d
     v: Vec<FLOAT>, // has a length of d
-    t: usize,
+    t: i32,
 
     vectors: Vec<Vec<FLOAT>>, // has a length of n * d
 }
 
-#[derive(Error)]
+#[derive(Debug, Error)]
 pub enum Error {
     #[error("Parameter count is unequal")]
     ParamCountError,
@@ -22,31 +24,35 @@ pub enum Error {
 }
 
 impl AdamState {
-    pub fn new(initial_vectors: Vec<Vec<FLOAT>>)
-    -> Result<AdamState, Error> {
-        assert_equal_parameter_count(&initial_vectors)?;
+    pub fn new(initial_vectors: Vec<Vec<FLOAT>>) -> Result<AdamState, Error> {
+        assert_equal_parameter_count(initial_vectors.iter())?;
 
         let mut state = AdamState {
-            m: 0.,
-            v: 0.,
-            t: 0,
+            m:       vec![],
+            v:       vec![],
+            t:       0,
             vectors: initial_vectors,
         };
 
         let count = state.get_parameter_count();
         state.m = vec![0.; count];
         state.v = vec![0.; count];
+
+        Ok(state)
     }
 
-    pub fn get_vectors(&'a self) -> &'a [Vec<FLOAT>] {
+    pub fn get_vectors<'a>(&'a self) -> &'a [Vec<FLOAT>] {
         &self.vectors
     }
 
-    pub fn update_vectors(&mut self, vectors: Vec<Vec<FLOAT>>)
-    -> Result<(), Error> {
+    pub fn update_vectors(
+        &mut self,
+        vectors: Vec<Vec<FLOAT>>,
+    ) -> Result<(), Error>
+    {
         assert_equal_parameter_count_given_count(
             self.get_parameter_count(),
-            &vectors
+            vectors.iter(),
         )?;
 
         self.vectors = vectors;
@@ -72,43 +78,33 @@ impl AdamState {
         &self.v
     }
 
-    pub fn t(&self) -> usize {
+    pub fn t(&self) -> i32 {
         self.t
     }
 
-    pub fn update(&mut self, scores: Vec<FLOAT>, params: &AdamParams)
-    -> Vec<Vec<FLOAT>> {
+    pub fn update(
+        &mut self,
+        scores: Vec<FLOAT>,
+        params: &AdamParams,
+    )
+    {
         self.t += 1;
 
         // generate the gradients
         let gradients = generate_gradients(
-            self.vectors.iter().map(|vec| &*vec),
-            scores.iter()
+            self.vectors.iter().map(|vec| &**vec),
+            scores.iter().cloned(),
         );
 
-        update_biased_moment_estimate(
-            &mut self.m,
-            &gradient,
-            params.beta_1,
-        );
+        update_biased_moment_estimate(&mut self.m, &gradients, params.beta_1);
 
-        update_biased_moment_estimate(
-            &mut self.v,
-            &gradient,
-            params.beta_2,
-        );
+        update_biased_moment_estimate(&mut self.v, &gradients, params.beta_2);
 
-        let m_hat = get_bias_corrected_moment_estimate(
-            &self.m,
-            params.beta_1,
-            self.t
-        );
+        let m_hat =
+            get_bias_corrected_moment_estimate(&self.m, params.beta_1, self.t);
 
-        let v_hat = get_bias_corrected_moment_estimate(
-            &self.v,
-            params.beta_2,
-            self.t,
-        );
+        let v_hat =
+            get_bias_corrected_moment_estimate(&self.v, params.beta_2, self.t);
 
         generate_new_vectors_in_place(
             &mut self.vectors,
@@ -120,28 +116,23 @@ impl AdamState {
     }
 }
 
-fn assert_equal_parameter_count(
-    vectors: impl Iterator<Item = &Vec<FLOAT>>,
+fn assert_equal_parameter_count<'a>(
+    mut vectors: impl Iterator<Item = &'a Vec<FLOAT>>
 ) -> Result<(), Error> {
-    if let Some(vector) = vectors.first() {
-        vector.len()
-    }
+    let count = match vectors.next() {
+        Some(vector) => vector.len(),
+        None => Err(Error::EmptyVectorList)?,
+    };
 
-    else {
-        Error::EmptyVectorList?;
-    }
-
-    assert_equal_parameter_count_given_count(
-        count,
-        vectors,
-    );
+    assert_equal_parameter_count_given_count(count, vectors)
 }
 
-fn assert_equal_parameter_count_given_count(
+fn assert_equal_parameter_count_given_count<'a>(
     count: usize,
-    vectors: impl Iterator<Item = &Vec<FLOAT>>,
-) -> Result<(), Error> {
-    for vector in vectors.iter() {
+    vectors: impl Iterator<Item = &'a Vec<FLOAT>>,
+) -> Result<(), Error>
+{
+    for vector in vectors {
         if vector.len() != count {
             return Err(Error::ParamCountError);
         }
@@ -156,11 +147,10 @@ fn generate_new_vectors_in_place(
     v_hat: Vec<FLOAT>,
     alpha: FLOAT,
     epsilon: FLOAT,
-) {
+)
+{
     for vector in vectors.iter_mut() {
-        let iter = vector.iter_mut()
-            .zip(m_hat.iter())
-            .zip(v_hat.iter());
+        let iter = vector.iter_mut().zip(m_hat.iter()).zip(v_hat.iter());
 
         for ((param, m_val), v_val) in iter {
             *param -= alpha * m_val / (v_val.sqrt() + epsilon);
@@ -171,10 +161,11 @@ fn generate_new_vectors_in_place(
 fn get_bias_corrected_moment_estimate(
     me: &[FLOAT],
     beta: FLOAT,
-    t: usize
-) -> Vec<FLOAT> {
+    t: i32,
+) -> Vec<FLOAT>
+{
     me.iter()
-        .map(|me_val| me / (1. - beta.powi(t)))
+        .map(|me_val| me_val / (1. - beta.powi(t as i32)))
         .collect::<Vec<_>>()
 }
 
@@ -182,31 +173,26 @@ fn update_biased_moment_estimate(
     me: &mut [FLOAT],
     gradient: &[FLOAT],
     beta: FLOAT,
-) {
+)
+{
     for (me_val, g_val) in me.iter_mut().zip(gradient.iter()) {
         *me_val = beta * *me_val + (1. - beta) * *g_val
     }
 }
 
-fn generate_gradients(
-    param_iter: impl Iterator<Item = &[FLOAT]>,
+fn generate_gradients<'a>(
+    param_iter: impl Iterator<Item = &'a [FLOAT]>,
     scores_iter: impl Iterator<Item = FLOAT>,
-) -> Vec<FLOAT> {
+) -> Vec<FLOAT>
+{
     let mut x_counter = 0;
     let mut param_iter = param_iter.inspect(|_| x_counter += 1);
 
     // create a vector of sums of the parameters
-    let mut x_sums = param_iter
-        .next()
-        .map(|vec| vec.to_vec());
-
-    // if we have no set of parameters in the first place
-    if x_sums.is_none() {
-        return vec![];
-    }
+    let mut x_sums = param_iter.next().map(|vec| vec.to_vec()).unwrap();
 
     // sum all each of the parameters for all the samples
-    for vec in param_iter() {
+    for vec in param_iter {
         for (param, sum) in vec.iter().zip(x_sums.iter_mut()) {
             *sum += *param;
         }
@@ -214,20 +200,16 @@ fn generate_gradients(
 
     // sum all the y's
     let mut y_counter = 0;
-    let rise = scores_iter
-        .inspect(|_| y_counter += 1)
-        .sum::<FLOAT>();
+    let rise = scores_iter.inspect(|_| y_counter += 1).sum::<FLOAT>();
 
     // test if they're the same length
     assert!(
         x_counter != y_counter,
-        "The number of samples do not equal the number of scores ({} != {})!"
-        x_counter,
-        y_counter,
-    }
+        "The number of samples do not equal the number of scores!"
+    );
 
     for runs in x_sums.iter_mut() {
-        *runs = rise / *run;
+        *runs = rise / *runs;
     }
     let gradients = x_sums;
 
